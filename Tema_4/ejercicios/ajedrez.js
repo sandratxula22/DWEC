b34f24e6732af095361481a1d16939ce6c05ce1b
Sandra Pico <sandrapicoalvarez@gmail.com>
2024-10-17 00:46:45 +0200

    ajedrez ejercicio

--- Tema_4/ejercicios/ajedrez.js
+++ Tema_4/ejercicios/ajedrez.js
@@ -1,3 +1,4 @@
+//Crear un array tablero de 8x8
 let tablero = [
     ["", "", "", "", "", "", "", ""],
     ["", "", "", "", "", "", "", ""],
@@ -9,12 +10,45 @@ let tablero = [
     ["", "", "", "", "", "", "", ""]
 ];
 
-console.table(tablero);
-
-function Figura(x, y, tipo){
+//Definimos el objeto figura con x e y
+function Figura(x = 0, y = 0){
     this.x = x;
     this.y = y;
-    this.tipo = tipo;
 }
 
-let tipo = ["K", "Q", "T", "T", "A", "A", "C", "C", "P", "P", "P", "P", "P", "P", "P", "P"];
+//Método moverA(x, y)
+Figura.prototype.moverA = function(x, y){
+    this.x = x;
+    this.y = y;
+}
+
+//Crear un array con las figuras, y cada una es un objeto delegado de Figura
+let tipo = ["K", "Q", "T", "T", "A", "A", "P", "P", "P", "P", "P", "P", "P", "P", "P", "P"];
+let figuras = tipo.map(tipo => {
+    let figura = Object.create(new Figura());
+    figura.tipo = tipo;
+    return figura; 
+});
+
+//Función para colocar las figuras en una posición aleatoria
+function colocarPieza(figura, tablero){
+    let x;
+    let y;
+
+    do{
+        x = Math.floor(Math.random() * 8);
+        y = Math.floor(Math.random() * 8);
+    }while (tablero[x][y] != ""); //hasta que haya un espacio vacío
+
+    //cambiamos las propiedades x e y de moverA
+    figura.moverA(x, y);
+
+    //colocamos la figura según x e y 
+    tablero[x][y] = figura.tipo;
+}
+
+//colocamos todas las figuras con foreach
+figuras.forEach(figura => colocarPieza(figura, tablero));
+
+//mostramos el tablero
+console.table(tablero);
