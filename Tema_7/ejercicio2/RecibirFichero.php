e350edadb50b11d11a785a087cab2937600613a5
sandratxula22 <sandra.picalv.1@iesjulianmarias.es>
2024-12-17 01:43:38 +0100

    ejercicio2

---
+++ Tema_7/ejercicio2/RecibirFichero.php
@@ -0,0 +1,9 @@
+<?php
+$fichero_subido = '.\\'.basename($_FILES['mifichero']['name']);
+if(move_uploaded_file($_FILES['mifichero']['tmp_name'], $fichero_subido)){
+    echo basename($fichero_subido);
+}else{
+    echo "Error";
+}
+
+?>
